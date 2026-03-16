b811598f8fc370425c32740fe22104ef92295ee4
nic-at-voyagers <nam070108@gmail.com>
2026-03-16 18:27:36 +0700

    Actually disabled autocmp fr this time

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
-vim.g.lazyvim_cmp = "nvim-cmp"
+vim.g.lazyvim_cmp = "blink"
 vim.g.autoformat = false
 vim.opt.tabstop = 8
 vim.opt.shiftwidth = 8
